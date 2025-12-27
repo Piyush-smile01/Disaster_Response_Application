f76b4cebdc691c15d6a84eb07e7685997a069c2d
Piyush-smile01 <jhambpiyush12@gmail.com>
2025-12-27 16:24:16 +0530

    update

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,8 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./disaster-management-aaf3e-57149546527b.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use("/sos", sosRoutes);
 
-const PORT = 3000;
+const PORT =process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
